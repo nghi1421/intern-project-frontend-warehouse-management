ac0ce08c0d7e98ddb1328d4ddd535d7c28fe17d3
Nguyen Thanh Nghi <thanhnghi1421@gmail.com>
2023-07-05 01:12:18 +0700

    set up color for tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,28 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme')
+const colors = require('tailwindcss/colors')
+
 module.exports = {
-  content: [],
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, 
   theme: {
+    fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+    },
+    colors: {
+      ...colors,
+      primary: colors.blue,
+      secondary: colors.indigo,
+      danger: colors.red,
+      warning: colors.amber,
+      transparent: 'transparent',
+      current: 'currentColor',
+    },
     extend: {},
   },
-  plugins: [],
+  variants: {
+    extend: {},
+  },
+  plugins: []
 }
 
